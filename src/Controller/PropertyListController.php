e283c3beffc1a0d915f1a70ff29f89e29090e8ab
vuras <dvarnius@gmail.com>
2018-01-20 19:47:16 +0200

    FromToForm implemented

--- src/Controller/PropertyListController.php
+++ src/Controller/PropertyListController.php
@@ -10,7 +10,9 @@ namespace Drupal\property_list\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\property_list\Endpoint\PropertiesEndpoint;
+use Drupal\property_list\Form\FromToForm;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class PropertyListController extends ControllerBase
 {
@@ -44,11 +46,22 @@ class PropertyListController extends ControllerBase
    *
    * @return array
    */
-  public function content() {
-    $response = $this->endpoint->get();
+  public function content(Request $request)
+  {
+    $form = \Drupal::formBuilder()->getForm(FromToForm::class);
+
+    $from = $request->query->get('from', date('Ymd'));
+    $to = $request->query->get('to', date('Ymd', strtotime('last day of december this year')));
+    $response = $this->endpoint->get([
+      'from' => $from,
+      'to' => $to
+    ]);
+
     return array(
-      '#theme' => 'property_list',
+      '#theme'      => 'property_list',
       '#properties' => $response['results'],
+      '#form'       => $form,
+      '#empty'      => 'We could not find any available properties for these dates'
     );
   }
 }
---
+++ src/Form/FromToForm.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arturas
+ * Date: 2018-01-20
+ * Time: 16:26
+ */
+
+namespace Drupal\property_list\Form;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\property_list\Endpoint\PropertiesEndpoint;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class FromToForm extends FormBase
+{
+  /**
+   * @inheritdoc
+   * @return string
+   */
+  public function getFormId()
+  {
+    return 'property_list_form';
+  }
+
+  /**
+   * @param array $form
+   * @param FormStateInterface $form_state
+   * @return array
+   */
+  public function buildForm(array $form, FormStateInterface $form_state)
+  {
+    $form['from'] = [
+      '#type'             => 'date',
+      '#title'            => 'From'
+    ];
+
+    $form['to'] = [
+      '#type'             => 'date',
+      '#title'            => 'To'
+    ];
+
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Search'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * @param array $form
+   * @param FormStateInterface $form_state
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+    if(empty($form_state->getValue('from'))){
+      $form_state->setErrorByName('from', $this->t('From date is empty'));
+    }
+    if(empty($form_state->getValue('to'))){
+      $form_state->setErrorByName('to', $this->t('To date is empty'));
+    }
+  }
+
+  /**
+   * @param array $form
+   * @param FormStateInterface $form_state
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state)
+  {
+    $from = date('Ymd', strtotime($form_state->getValue('from')));
+    $to = date('Ymd', strtotime($form_state->getValue('to')));
+
+    $url = Url::fromRoute(
+      'property_list.content',
+      [],
+      [
+        'query' => [
+          'from' => $from, 'to' => $to
+        ]
+      ]);
+
+    $form_state->setRedirectUrl($url);
+  }
+
+}
